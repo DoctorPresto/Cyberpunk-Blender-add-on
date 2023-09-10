933dba6e1047fab5fb5a672d197e629f68470f6c
DoctorPresto <111536029+DoctorPresto@users.noreply.github.com>
2023-09-10 18:24:36 -0400

    changes

--- i_scene_cp77_gltf/__init__.py
+++ i_scene_cp77_gltf/__init__.py
@@ -1,15 +1,3 @@
-bl_info = {
-    "name": "Cyberpunk 2077 IO Suite",
-    "author": "HitmanHimself, Turk, Jato, dragonzkiller, kwekmaster, glitchered, Simarilius, The Magnificent Doctor Presto",
-    "version": (1,4, 0),
-    "blender": (3, 1, 0),
-    "location": "File > Import-Export",
-    "description": "Import and Export WolvenKit Cyberpunk2077 gLTF models with materials, Import .streamingsector and .ent from .json",
-    "warning": "",
-    "category": "Import-Export",
-}
-
-
 import bpy
 import bpy.utils.previews
 import json
@@ -29,11 +17,78 @@ from .main.entity_import import *
 from .main.attribute_import import manage_garment_support
 from .main.sector_import import *
 from bpy_extras.io_utils import ExportHelper
-from .main.exporters import *
+from .exporters.glb_export import *
+from .exporters.hp_export import *
+from .exporters.collision_export import *
+from .exporters.mlsetup_export import *
 from .main.common import json_ver_validate
+<<<<<<< Updated upstream
+=======
+from .main.collisions import CP77CollisionGen
+from .main.animtools import play_anim 
+#from . import addon_updater_ops
 
-icons_dir = os.path.join(os.path.dirname(__file__), "icons")
-custom_icon_col = {}
+
+bl_info = {
+    "name": "Cyberpunk 2077 IO Suite",
+    "author": "HitmanHimself, Turk, Jato, dragonzkiller, kwekmaster, glitchered, Simarilius, The Magnificent Doctor Presto",
+    "version": (1, 4, 0),
+    "blender": (3, 1, 0),
+    "location": "File > Import-Export",
+    "description": "Import and Export WolvenKit Cyberpunk2077 gLTF models with materials, Import .streamingsector and .ent from .json",
+    "warning": "",
+    "category": "Import-Export", }
+
+
+### plugin preferences class to allow for personalized used settings
+#@addon_updater_ops.make_annotations
+class CP77IOSuitePreferences(bpy.types.AddonPreferences):
+    bl_idname = __name__
+
+    experimental_features: bpy.props.BoolProperty(
+    name= "Enable Experimental Features",
+    description="Experimental Features for Mod Developers, may encounter bugs",
+    default=False,
+    )
+    
+ ## toggle the mod tools tab and its sub panels - default True
+    show_modtools: bpy.props.BoolProperty(
+    name= "Show the Mod Tools Panel",
+    description="Show the Mod tools Tab in the 3d viewport",
+    default=True,
+    )
+
+    show_meshtools: bpy.props.BoolProperty(
+    name= "Show the Mesh Tools Panel",
+    description="Show the mesh tools panel",
+    default=True,
+    )
+
+    show_collisiontools: bpy.props.BoolProperty(
+    name= "Show the Collision Tools Panel",
+    description="Show the Collision tools panel",
+    default=True,
+    )
+
+    show_animtools: bpy.props.BoolProperty(
+    name= "Show the Animation Tools Panel",
+    description="Show the anim tools panel",
+    default=True,
+    )
+
+
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, "experimental_features")
+        layout.prop(self, "show_modtools")
+        if self.show_modtools:
+            layout.use_property_split = True
+            layout.prop(self, "show_meshtools")
+            layout.prop(self, "show_collisiontools")
+            layout.prop(self, "show_animtools")
+        #addon_updater_ops.update_settings_ui(self,context)
+>>>>>>> Stashed changes
+
 
 def SetCyclesRenderer(set_gi_params=False):
     # set the render engine for all scenes to Cycles
@@ -53,15 +108,167 @@ def SetCyclesRenderer(set_gi_params=False):
         cycles.use_fast_gi = False
         cycles.ao_bounces = 1
         cycles.ao_bounces_render = 1
+       
 
+icons_dir = os.path.join(os.path.dirname(__file__), "icons")
+custom_icon_col = {}
+
+
+### allow deleting animations from the animset panel, regardless of editor context
+class CP77AnimsDelete(bpy.types.Operator):
+
+    bl_idname = 'cp77.delete_anims'
+    bl_label = "Delete an action from the animslist"
+    bl_options = {'INTERNAL', 'UNDO'}
+
+    name: bpy.props.StringProperty()
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object and context.active_object.animation_data
+
+    def execute(self, context):
+        obj = context.active_object
+        action = bpy.data.actions.get(self.name, None)
+        if not action:
+            return {'CANCELLED'}
+
+        bpy.data.actions.remove(action)
+
+        return {'FINISHED'}
+    
+
+## this class is where most of the function is so far - play/pause 
+## Todo: fix renaming actions from here
+class CP77Animset(bpy.types.Operator):
+
+    bl_idname = 'cp77.set_animset'
+    bl_label = "Available Animsets"
+    bl_options = {'INTERNAL', 'UNDO'}
+
+    name: bpy.props.StringProperty(options={'HIDDEN'})
+    new_name: bpy.props.StringProperty(name="New name", default="")
+    play: bpy.props.BoolProperty(options={'HIDDEN'}, default=False)
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object and context.active_object.animation_data
+    
+    def execute(self, context):
+        obj = context.active_object
+        if obj and obj.type == 'ARMATURE':
+            if self.play:
+                # Pass the animation name to the play_anim function
+                play_anim(self, context, self.name)
+            return {'FINISHED'}
+
+### Draw a panel within the modtools tree to store anims functions
+class CP77_PT_AnimsPanel(bpy.types.Panel):
+    bl_parent_id = "CP77_PT_modtools"
+    bl_idname = "CP77_PT_animspanel"
+    bl_space_type = "VIEW_3D"
+    bl_label = "Animation Tools"
+    bl_region_type = "UI"
+    bl_category = "CP77 Modding"
+
+    name: bpy.props.StringProperty(options={'HIDDEN'})
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object and context.active_object.type == 'ARMATURE'
+    
+
+## make sure the context is unrestricted as possible, ensure there's an armature selected 
+    def draw(self, context):
+        layout = self.layout 
+
+        cp77_addon_prefs = context.preferences.addons[__name__].preferences
+
+        if cp77_addon_prefs.show_animtools:
+            if bpy.context.mode in {'OBJECT', 'POSE', 'EDIT_ARMATURE'}:
+                obj = bpy.context.active_object
+                if obj and obj.type == 'ARMATURE':
+                    available_anims = bpy.data.actions
+                    active_action = obj.animation_data.action if obj.animation_data else None
+
+                    box = layout.box()
+                    row = box.row(align=True)
+                    row.label(text='Animsets', icon_value=custom_icon_col["import"]['WKIT'].icon_id)
+                    if available_anims:
+                        col = box.column(align=True)
+                        for action in available_anims:
+                            selected = action == active_action
+                            row = col.row(align=True)
+                            sub = row.column(align=True)
+                            sub.ui_units_x = 1.0
+                            if selected and context.screen.is_animation_playing:
+                                op = sub.operator('screen.animation_cancel', icon='PAUSE', text="", emboss=False)
+                                op.restore_frame = False
+                            else:
+                                icon = 'PLAY' if selected else 'TRIA_RIGHT'
+                                op = sub.operator('cp77.set_animset', icon=icon, text="", emboss=False)
+                                op.name = action.name
+                                op.play = True
+
+                                op = row.operator('cp77.set_animset', text=action.name)
+                                op.name = action.name
+                                op.play = False
+                                row.operator('cp77.delete_anims', icon='X', text="").name = action.name
+
+<<<<<<< Updated upstream
+=======
+
+class CP77CollisionGenerator(bpy.types.Operator):
+    bl_idname = "generate_cp77.collisions"
+    bl_parent_id = "CP77_PT_collisions"
+    bl_label = "Generate Convex Collider"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "CP77 Modding"
+    bl_options = {'REGISTER'}
+
+    def draw(self, context):
+        layout = self.layout
+
+    def execute(self, context):
+        props = context.scene.cp77_collision_tools_panel_props
+        CP77CollisionGen(context, props.sampleverts)
+        return {"FINISHED"}
+        
+
+class CollectionAppearancePanel(bpy.types.Panel):
+    bl_label = "Ent Appearances"
+    bl_idname = "PANEL_PT_appearance_variants"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "collection"
+
+    #only draw the if the collector has an appearanceName property
+    @classmethod
+    def poll(cls, context):
+        collection = context.collection
+        return hasattr(collection, "appearanceName")
+
+    def draw(self, context):
+        layout = self.layout
+        collection = context.collection
+        layout.prop(collection, "appearanceName")
+    
+
+>>>>>>> Stashed changes
 class CP77CollisionExport(bpy.types.Operator):
     bl_idname = "export_scene.collisions"
     bl_label = "Export Collisions to .JSON"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "CP77 Modding"
+<<<<<<< Updated upstream
     bl_parent_id = "CP77_PT_CollisionTools"
     
+=======
+    bl_parent_id = "CP77_PT_collisions"
+
+>>>>>>> Stashed changes
     filepath: bpy.props.StringProperty(subtype="FILE_PATH")
 
     def execute(self, context):
@@ -75,57 +282,172 @@ class CP77CollisionExport(bpy.types.Operator):
     def draw(self, context):
         layout = self.layout
 
+<<<<<<< Updated upstream
 class CP77_PT_CollisionTools(bpy.types.Panel):
     bl_parent_id = "CP77_PT_ModTools"
     bl_label = "Collision Tools"
     bl_idname = "CP77_PT_CollisionTools"
+=======
+
+# class CP77MassExport(bpy.types.Operator):
+#     bl_idname = "export_scene.all"
+#     bl_label = "Export Ent Meshes"
+#     bl_space_type = "VIEW_3D"
+#     bl_region_type = "UI"
+#     bl_category = "CP77 Modding"
+#     bl_parent_id = "CP77_PT_MeshTools"
+  
+#     def execute(self, context):
+#         ExportAll(self, context)
+#         return {"FINISHED"}
+
+#     def draw(self, context):
+#         layout = self.layout
+
+
+class CP77HairProfileExport(bpy.types.Operator):
+    bl_idname = "export_scene.hp"
+    bl_label = "Export Hair Profile"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "CP77 Modding"
+    bl_parent_id = "CP77_PT_MeshTools"
+
+    filepath: bpy.props.StringProperty(subtype="FILE_PATH")
+  
+    def execute(self, context):
+        cp77_hp_export(self.filepath)
+        return {"FINISHED"}
+
+    def draw(self, context):
+        layout = self.layout
+
+class CP77MlSetupExport(bpy.types.Operator):
+    bl_idname = "export_scene.mlsetup"
+    bl_label = "Export MLSetup"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "CP77 Modding"
+    bl_parent_id = "CP77_PT_MeshTools"
+
+    filepath: bpy.props.StringProperty(subtype="FILE_PATH")
+  
+    def execute(self, context):
+        cp77_mlsetup_export(self, context)
+        return {"FINISHED"}
+
+    def draw(self, context):
+        layout = self.layout
+
+
+class CP77_PT_CollisionToolsPanelProps(bpy.types.PropertyGroup):
+    sampleverts: bpy.props.StringProperty(
+        name="Vertices to Sample",
+        description="This is the number of vertices in your new collider",
+        default="100",
+        maxlen=3 
+    )
+
+
+class CP77SetArmature(bpy.types.Operator):
+    bl_idname = "cp77.set_armature"
+    bl_label = "Change Armature Target"
+    
+    def execute(self, context):
+        target_armature_name = context.scene.selected_armature
+        target_armature = bpy.data.objects.get(target_armature_name)
+        if target_armature and target_armature.type == 'ARMATURE':
+            for obj in bpy.context.selected_objects:
+                if obj.type == 'MESH':
+                    for modifier in obj.modifiers:
+                        if modifier.type == 'ARMATURE':
+                            modifier.object = target_armature
+            return {'FINISHED'}
+    
+    
+def CP77ArmatureList(self, context):
+    items = []
+    for obj in bpy.data.objects:
+        if obj.type == 'ARMATURE':
+            items.append((obj.name, obj.name, ""))
+    return items
+
+
+class CP77_PT_MeshTools(bpy.types.Panel):
+    bl_parent_id = "CP77_PT_modtools"
+    bl_label = "Mesh Tools"
+    bl_idname = "CP77_PT_MeshTools"
+>>>>>>> Stashed changes
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "CP77 Modding"
+   
+    @classmethod
+    def poll(cls, context):
+        return context.active_object and context.active_object.type == 'MESH'
+
+    def draw(self, context):
+        layout = self.layout
+
+        cp77_addon_prefs = context.preferences.addons[__name__].preferences
+
+        if cp77_addon_prefs.show_meshtools:
+            row = layout.row()
+            row.label(text="Target Armature:")
+            row.prop(context.scene, "selected_armature", text="")
+            layout.operator("cp77.set_armature", text="Change Armature Targets")
+            layout.label(text="Material Exporters", icon="MATERIAL")
+            box = layout.box()
+            box.operator("export_scene.hp")
+            if context.preferences.addons[__name__].preferences.experimental_features:
+                box.operator("export_scene.mlsetup")
+        
+
+class CP77_PT_CollisionTools(bpy.types.Panel):
+    bl_parent_id = "CP77_PT_modtools"
+    bl_label = "Collision Tools"
+    bl_idname = "CP77_PT_collisions"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "CP77 Modding"
 
+    @classmethod
+    def poll(cls, context):
+        return context.active_object and context.active_object.type == 'MESH'
+
     def draw(self, context):
         layout = self.layout
+<<<<<<< Updated upstream
         layout.operator("export_scene.collisions")
+=======
+        props = context.scene.cp77_collision_tools_panel_props
+        cp77_addon_prefs = context.preferences.addons[__name__].preferences
+
+        if cp77_addon_prefs.show_collisiontools:
+            layout.operator("export_scene.collisions")
+            if context.mode == 'EDIT_MESH':
+                row = layout.row()
+                row.label(text="Vertices to Sample:")
+                row.prop(props, "sampleverts", text="")
+                layout.operator("generate_cp77.collisions")
+
+>>>>>>> Stashed changes
 
 class CP77_PT_ModTools(bpy.types.Panel):
     bl_label = "Cyberpunk Modding Tools"
-    bl_idname = "CP77_PT_ModTools"
+    bl_idname = "CP77_PT_modtools"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_options = {"DEFAULT_CLOSED"}
-    bl_category = "CP77 Modding"
+    bl_category = "CP77Modding"
+
+    @classmethod
+    def poll(cls, context):
+        addon_prefs = context.preferences.addons[__name__].preferences
+        return addon_prefs.show_modtools
 
     def draw(self, context):
         layout = self.layout
-
-## these will be used later 
-# class CP77_PT_rigview(bpy.types.Panel):
-#     bl_parent_id = "CP77_PT_ModTools"
-#     bl_space_type = "VIEW_3D"
-#     bl_region_type = "UI"
-#     bl_label = "Cyberpunk Rigview"
-#     bl_category = "CP77 Modding"
-    
-    # def poll(cls, context):
-    #     return (context.active_object
-    #         and context.active_object.type == 'ARMATURE'
-    #         and context.mode in {'OBJECT', 'POSE', 'EDIT_ARMATURE'}
-    #         and cls.draw_funcs)
-
-#     def draw(self, context):
-#         layout = self.layout
-
-
-# class CP77_PT_AnimsPanel(bpy.types.Panel):
-#     bl_parent_id = "CP77_PT_ModTools"
-#     bl_idname = "CP77_PT_AnimsPanel"
-#     bl_space_type = "VIEW_3D"
-#     bl_label = "Animsets"
-#     bl_region_type = "UI"
-#     bl_category = "CP77 Modding"
-
-#     def draw(self, context):
-#         layout = self.layout
 
 
 ## adds a message box for the exporters to use for error notifications, will also be used later for redmod integration    
@@ -351,7 +673,12 @@ class CP77Import(bpy.types.Operator,ImportHelper):
                 #print('o.name - ',o.name)
                 if 'Armature' in o.name:
                     o.hide_set(self.hide_armatures)
+<<<<<<< Updated upstream
             collection['orig_filepath']=filepath
+=======
+
+            collection['orig_filepath']=filepath    
+>>>>>>> Stashed changes
             for name in bpy.data.materials.keys():
                 if name not in existingMaterials:
                     bpy.data.materials.remove(bpy.data.materials[name], do_unlink=True, do_id_user=True, do_ui_user=True)
@@ -469,15 +796,22 @@ class CP77Import(bpy.types.Operator,ImportHelper):
                         index = index + 1
                         
         return {'FINISHED'}
+    
+       
 
 def menu_func_import(self, context):
     self.layout.operator(CP77Import.bl_idname, text="Cyberpunk GLTF (.gltf/.glb)", icon_value=custom_icon_col["import"]['WKIT'].icon_id)
     self.layout.operator(CP77EntityImport.bl_idname, text="Cyberpunk Entity (.json)", icon_value=custom_icon_col["import"]['WKIT'].icon_id)
     self.layout.operator(CP77StreamingSectorImport.bl_idname, text="Cyberpunk StreamingSector", icon_value=custom_icon_col["import"]['WKIT'].icon_id)
 
+
 def menu_func_export(self, context):
     self.layout.operator(CP77GLBExport.bl_idname, text="Export Selection to GLB for Cyberpunk", icon_value=custom_icon_col["import"]['WKIT'].icon_id)
     
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 #kwekmaster - Minor Refactoring 
 classes = (
     CP77Import,
@@ -487,33 +821,71 @@ classes = (
     CP77GLBExport,
     ShowMessageBox,
     CP77_PT_ModTools,
-    #CP77_PT_AnimsPanel,
-    #CP77_PT_rigview,
+    CP77_PT_AnimsPanel,
     CP77_PT_CollisionTools,
+<<<<<<< Updated upstream
     CP77CollisionExport
 )
+=======
+    CP77CollisionExport,
+    CP77CollisionGenerator,
+    CP77Animset,
+    CP77AnimsDelete,
+    CP77IOSuitePreferences,
+    CollectionAppearancePanel,
+    CP77HairProfileExport,
+    CP77_PT_MeshTools,
+#    CP77MassExport,
+    CP77SetArmature,
+    CP77_PT_CollisionToolsPanelProps,
+    CP77MlSetupExport,
+    )
+
+>>>>>>> Stashed changes
 
 def register():
     custom_icon = bpy.utils.previews.new()
     custom_icon.load("WKIT", os.path.join(icons_dir, "wkit.png"), 'IMAGE')
     custom_icon_col["import"] = custom_icon
-    
+    #addon_updater_ops.register(bl_info)
+
+
     #kwekmaster - Minor Refactoring 
     for cls in classes:
         bpy.utils.register_class(cls)
-        
+
+    bpy.types.Scene.selected_armature = bpy.props.EnumProperty(items=CP77ArmatureList)
+    bpy.types.Scene.cp77_collision_tools_panel_props = bpy.props.PointerProperty(type=CP77_PT_CollisionToolsPanelProps)           
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
     bpy.types.TOPBAR_MT_file_export.append(menu_func_export) 
+<<<<<<< Updated upstream
     
+=======
+
+  
+>>>>>>> Stashed changes
 def unregister():
     bpy.utils.previews.remove(custom_icon_col["import"])
+    del bpy.types.Scene.cp77_collision_tools_panel_props
+    del bpy.types.Scene.selected_armature
     
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     #kwekmaster - Minor Refactoring 
     for cls in classes:
         bpy.utils.unregister_class(cls)
         
+    #addon_updater_ops.unregister()
+
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
     bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
+<<<<<<< Updated upstream
                 
+=======
+    
+
+>>>>>>> Stashed changes
 if __name__ == "__main__":
     register()
